767b2f38a87033bbf414f45c7fde19e2f1bf83ab
olliez-mods <socksinthewash@gmail.com>
2024-03-24 13:26:08 +1100

    Added TODO items

---
+++ ServerTools.ps1
@@ -0,0 +1,7 @@
+# TODO Let user choose option
+# TODO make function to verify existence of needed folders and 
+# files (see if we have a volume, if servers been built, etc.)
+# TODO Allow user to copy world files / import world files (overwrite)
+# TODO Let user see all settings (grouped by catagory) and edit each one
+# let user see specifics about each setting and see all related commands (eg. EVE X, EVE Y, FORCE EVE)
+# let user see logs and server specific details.
