73c3c5808870a6460d649e20625086b4dd19dcf1
olliez-mods <socksinthewash@gmail.com>
2024-03-29 11:34:04 -0700

    Replace echo with WriteOutput

--- ServerTools.ps1
+++ ServerTools.ps1
@@ -10,7 +10,7 @@ $iniFilePath = "ContainerConfig.ini"
 
 # Create an empty hashtable to store key-value pairs
 $iniConfig = @{}
-echo "Loading Config file [$iniFilePath]..."
+Write-Output "Loading Config file [$iniFilePath]..."
 # Read the .ini file line by line
 Get-Content -Path $iniFilePath | ForEach-Object {
     # Remove leading and trailing whitespace
@@ -27,8 +27,8 @@ Get-Content -Path $iniFilePath | ForEach-Object {
 }
 
 $VolumePath = $iniConfig['VolumePath']
-echo "Loaded VolumePath:$VolumePath"
-echo "  (Full Path [$PWD\$VolumePath])"
+Write-Output "Loaded VolumePath:$VolumePath"
+Write-Output "  (Full Path [$PWD\$VolumePath])"
 
 # =======================
 
@@ -40,46 +40,50 @@ $OneLifeData7Exists = Test-Path "$PWD/$VolumePath/OneLifeData7"
 $OneLifeServerExists = Test-Path "$PWD/$VolumePath/OneLife/server/"
 $OneLifeServerAppExists = Test-Path "$PWD/$VolumePath/OneLife/server/OneLifeServer" # file
 
-echo ""
-echo ""
+Write-Output ""
+Write-Output ""
 
 # If any of them dont exist, throw an error message and exit
 if(-not $volumeFolderExists){
-    echo "Volume folder was not found, make sure you have built the server before using ServerTools"
+    Write-Output "Volume folder was not found, make sure you have built the server before using ServerTools"
     Read-Host "."
     exit
 }
 if((-not $minorGemsExist) -or (-not $OneLifeExists) -or (-not $OneLifeData7Exists)){
-    echo "One or all of the main folders (OneLife/MinorGems/OneLifeData7) was not found, make sure that the server built successfully before using ServerTools"
+    Write-Output "One or all of the main folders (OneLife/MinorGems/OneLifeData7) was not found, make sure that the server built successfully before using ServerTools"
     Read-Host "."
     exit
 }
 if(-not $OneLifeServerExists){
-    echo "The server folder (./$VolumePath/OneLife/server) cannot be found, make sure that the server built successfully before using ServerTools"
+    Write-Output "The server folder (./$VolumePath/OneLife/server) cannot be found, make sure that the server built successfully before using ServerTools"
     Read-Host "."
     exit
 }
 if(-not $OneLifeServerAppExists){
-    echo "Could not find OneLifeServer application (./$VolumePath/OneLife/server/OneLifeServer), make sure that the server built successfully before using ServerTools"
+    Write-Output "Could not find OneLifeServer application (./$VolumePath/OneLife/server/OneLifeServer), make sure that the server built successfully before using ServerTools"
     Read-Host "."
     exit
 }
 
 # Menu
-function Print-Menu {
-    echo ""
-    echo ""
-    echo ""
-    echo "Welcome to ServerTools by Oliver, please choose an option:"
-    echo "1: Copy World Files"
-    echo "2: Import World Files"
-    echo "3: View server settings"
+function Write-Menu {
+    Write-Output ""
+    Write-Output ""
+    Write-Output ""
+    Write-Output "Welcome to ServerTools by Oliver, please choose an option:"
+    Write-Output "1: Copy World Files"
+    Write-Output "2: Import World Files"
+    Write-Output "3: View server settings"
 }
 
 Clear-Host
-Print-Menu
+Write-Menu
 $option = Read-Host ":"
 
-
-echo $option
-$hi = Read-Host "hi"
+if("$option" -eq "1"){
+    . "./scripts/getWorldFiles.ps1"
+    $worldFiles = Get-WorldFiles
+    foreach ($fileName in $worldFiles) {
+        Write-Output "hello $fileName"
+    }
+}
