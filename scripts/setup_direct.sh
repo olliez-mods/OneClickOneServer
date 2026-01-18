5bf59fa607ab5295f1f08b8dc7884bffc7106d82
Antinoid <socksinthewash@gmail.com>
2026-01-18 10:16:07 -0500

    g++ not gcc

--- scripts/setup_direct.sh
+++ scripts/setup_direct.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Ensure required packages are installed
-REQUIRED_PKGS=(git make gcc)
+REQUIRED_PKGS=(git make g++)
 MISSING_PKGS=()
 for pkg in "${REQUIRED_PKGS[@]}"; do
     if ! command -v $pkg >/dev/null 2>&1; then
